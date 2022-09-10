d2a1e3adeac2da638f0d7f6d69797cfeafb15122
Liu <hanquanl0115@gmail.com>
2022-09-10 16:20:45 -0400

    20220910

---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/schedule/ScheduleController.java
@@ -0,0 +1,64 @@
+package com.udacity.jdnd.course3.critter.schedule;
+
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.service.SchedulesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Handles web requests related to Schedules.
+ */
+@RestController
+@RequestMapping("/schedule")
+public class ScheduleController {
+
+    @Autowired
+    private SchedulesService schedulesService;
+
+    @PostMapping
+    public ScheduleDTO createSchedule(@RequestBody ScheduleDTO scheduleDTO) {
+        Schedule schedule = new Schedule();
+        schedule.setDate(scheduleDTO.getDate());
+        schedule.setActivities(scheduleDTO.getActivities());
+        return getScheduleDTO(schedulesService.saveSchedule(schedule, scheduleDTO.getEmployeeIds(), scheduleDTO.getPetIds()));
+    }
+
+    @GetMapping
+    public List<ScheduleDTO> getAllSchedules() {
+        List<Schedule> schedules = schedulesService.getAllSchedules();
+        return schedules.stream().map(this::getScheduleDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/pet/{petId}")
+    public List<ScheduleDTO> getScheduleForPet(@PathVariable long petId) {
+        List<Schedule> schedules = schedulesService.getAllSchedulesForPet(petId);
+        return schedules.stream().map(this::getScheduleDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public List<ScheduleDTO> getScheduleForEmployee(@PathVariable long employeeId) {
+        List<Schedule> schedules = schedulesService.getAllSchedulesForEmployee(employeeId);
+        return schedules.stream().map(this::getScheduleDTO).collect(Collectors.toList());
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public List<ScheduleDTO> getScheduleForCustomer(@PathVariable long customerId) {
+        List<Schedule> schedules = schedulesService.getAllSchedulesForCustomer(customerId);
+        return schedules.stream().map(this::getScheduleDTO).collect(Collectors.toList());
+    }
+
+    private ScheduleDTO getScheduleDTO(Schedule schedule) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO();
+        scheduleDTO.setId(schedule.getId());
+        scheduleDTO.setEmployeeIds(schedule.getEmployees().stream().map(Employee::getId).collect(Collectors.toList()));
+        scheduleDTO.setPetIds(schedule.getPets().stream().map(Pet::getId).collect(Collectors.toList()));
+        scheduleDTO.setDate(schedule.getDate());
+        scheduleDTO.setActivities(schedule.getActivities());
+        return scheduleDTO;
+    }
+}
---
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/SchedulesService.java
@@ -0,0 +1,57 @@
+package com.udacity.jdnd.course3.critter.service;
+
+import com.udacity.jdnd.course3.critter.entity.Customer;
+import com.udacity.jdnd.course3.critter.entity.Employee;
+import com.udacity.jdnd.course3.critter.entity.Pet;
+import com.udacity.jdnd.course3.critter.entity.Schedule;
+import com.udacity.jdnd.course3.critter.repository.CustomersRepository;
+import com.udacity.jdnd.course3.critter.repository.EmployeesRepository;
+import com.udacity.jdnd.course3.critter.repository.PetsRepository;
+import com.udacity.jdnd.course3.critter.repository.SchedulesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SchedulesService {
+
+    @Autowired
+    private SchedulesRepository schedulesRepository;
+
+    @Autowired
+    private PetsRepository petsRepository;
+
+    @Autowired
+    private EmployeesRepository employeesRepository;
+
+    @Autowired
+    private CustomersRepository customersRepository;
+
+    public List<Schedule> getAllSchedules() {
+        return schedulesRepository.findAll();
+    }
+
+    public List<Schedule> getAllSchedulesForPet(long petId) {
+        Pet pet = petsRepository.getOne(petId);
+        return schedulesRepository.getAllByPetsContains(pet);
+    }
+
+    public List<Schedule> getAllSchedulesForEmployee(long employeeId) {
+        Employee employee = employeesRepository.getOne(employeeId);
+        return schedulesRepository.getAllByEmployeesContains(employee);
+    }
+
+    public List<Schedule> getAllSchedulesForCustomer(long customerId) {
+        Customer customer = customersRepository.getOne(customerId);
+        return  schedulesRepository.getAllByPetsIn(customer.getPets());
+    }
+
+    public Schedule saveSchedule(Schedule schedule, List<Long> employeeIds, List<Long> petIds) {
+        List<Employee> employees = employeesRepository.findAllById(employeeIds);
+        List<Pet> pets = petsRepository.findAllById(petIds);
+        schedule.setEmployees(employees);
+        schedule.setPets(pets);
+        return schedulesRepository.save(schedule);
+    }
+}
