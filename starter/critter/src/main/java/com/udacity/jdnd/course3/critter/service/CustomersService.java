a468fb9ee27341612b6669a4bfc1eb44adcea1c0
Liu <hanquanl0115@gmail.com>
2022-09-10 21:06:07 -0400

    @transactional added

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomersService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/service/CustomersService.java
@@ -7,10 +7,12 @@ import com.udacity.jdnd.course3.critter.repository.PetsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Transactional
 @Service
 public class CustomersService {
 
